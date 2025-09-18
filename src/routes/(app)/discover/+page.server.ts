23d1d4eb78831b9bb866aac57e46783941149166
Leo Zheng <leozhengcs@gmail.com>
2025-09-17 17:55:19 -0700

    Add basic filter function

--- src/routes/(app)/discover/+page.server.ts
+++ src/routes/(app)/discover/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async({ locals: { supabase }}) => {
-
     const { data: { user } } = await supabase.auth.getUser();
 
     const { data, error } = await supabase
@@ -41,3 +40,4 @@ export const load: PageServerLoad = async({ locals: { supabase }}) => {
         // userChats: userChats.data
     };
 }
+
--- src/routes/(app)/discover/+page.svelte
+++ src/routes/(app)/discover/+page.svelte
@@ -6,7 +6,6 @@
 	import type { PublicUserProfile, UserProfile } from '$lib/types/Database.js';
 	import type { SupabaseClient } from '@supabase/supabase-js';
 	import { chatMap, openedModal, unreadMap } from '$lib/stores/globalStates.svelte.js';
-	import { get } from 'svelte/store';
 	import { fade } from 'svelte/transition';
 	import LoaderDiscover from '$lib/components/loaders/LoaderDiscover.svelte';
 
@@ -24,10 +23,22 @@
 	} = $derived(data);
 
 	let otherUsers: PublicUserProfile[] = $state([]);
+	let filteredUsers: PublicUserProfile[] = $state([]);
+	$inspect(filteredUsers);
 	let loading = $state(true);
+	let filter = $state('');
+
+	const searchUsers = (event: SubmitEvent) => {
+		event.preventDefault();
+		const normalized = filter.trim().toLowerCase();
+		if (!filter) filteredUsers = otherUsers;
+
+		filteredUsers = otherUsers.filter((user) => user.full_name.toLowerCase().includes(normalized));
+	};
 
 	onMount(async () => {
 		otherUsers = users.filter((u) => u.id !== user?.id);
+		filteredUsers = otherUsers;
 
 		const { data } = await supabase.auth.getSession();
 		const session = data.session;
@@ -53,11 +64,49 @@
 	{#if loading}
 		<LoaderDiscover />
 	{:else}
+		<form class="mb-5 max-w-md md:mb-10" onsubmit={searchUsers}>
+			<label
+				for="default-search"
+				class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white">Search</label
+			>
+			<div class="relative">
+				<div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+					<svg
+						class="h-4 w-4 text-gray-500 dark:text-gray-400"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 20 20"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+						/>
+					</svg>
+				</div>
+				<input
+					bind:value={filter}
+					type="search"
+					id="default-search"
+					class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+					placeholder="Search Users"
+				/>
+				<button
+					type="submit"
+					class="absolute end-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>Search</button
+				>
+			</div>
+		</form>
+
 		<div
 			class="z-[60] grid w-full grid-cols-1 place-items-center gap-10 pb-20 sm:grid-cols-2 md:grid-cols-3 md:pb-32 lg:grid-cols-4"
 			in:fade={{ duration: 1000 }}
 		>
-			{#each otherUsers as otherUser}
+			{#each filteredUsers as otherUser}
 				<UserCard user={otherUser} self={user!} {supabase} unread={$unreadMap[otherUser.id]} />
 			{/each}
 		</div>
